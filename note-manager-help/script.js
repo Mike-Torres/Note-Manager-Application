2f9fee9a937685773751d85d824819839f70807b
Michael Torres <michaelj.torres@yahoo.com>
2018-12-21 18:18:01 -0500

    third commit

---
+++ note-manager-help/script.js
@@ -0,0 +1,104 @@
+const ul = document.querySelector('ul');
+//-----------------------------------------------------
+//                     ADD ITEMS
+document.getElementById('add-btn').addEventListener('click', function(e){
+  e.preventDefault();
+
+   const addInput =  document.getElementById('add-input');
+   if(addInput.value !== ''){
+
+   const li = document.createElement('li'),
+   firstPar = document.createElement('p'),
+   secondPar = document.createElement('p'),
+   firstIcon = document.createElement('i'),
+   secondIcon = document.createElement('i'),
+   input = document.createElement('input');
+
+   firstIcon.className = "fa fa-pencil-square-o";
+   secondIcon.className = "fa fa-times";
+
+   input.className = 'edit-note';
+   input.setAttribute('type', 'text');
+
+   firstPar.textContent = addInput.value;
+
+   secondPar.appendChild(firstIcon);
+   secondPar.appendChild(secondIcon);
+   li.appendChild(firstPar);
+   li.appendChild(secondPar);
+   li.appendChild(input);
+   ul.appendChild(li);
+   addInput.value = '';
+ }
+});
+
+//---------------------------------------------------------
+//                       Delete Items
+ul.addEventListener('click', function(e){
+  if(e.target.classList[1] === "fa-pencil-square-o"){
+    const parentPar = e.target.parentNode;
+    parentPar.style.display = 'none';
+
+    const note = parentPar.previousElementSibling;
+    const input = parentPar.nextElementSibling;
+
+    input.style.display = 'block';
+    input.value = note.textContent;
+
+    input.addEventListener('keypress', function(e){
+    if(e.keyCode === 13){
+      if(input.value !== ''){
+        note.textContent = input.value;
+        parentPar.style.display = 'block';
+        input.style.display = 'none';
+      } else {
+        const li = input.parentNode;
+        li.parentNode.removeChild(li);
+      }
+    }
+  });
+} else if (e.target.classList[1] === "fa-times"){
+  const list = e.target.parentNode.parentNode;
+  list.parentNode.removeChild(list);
+}
+});
+
+//------------------------------------------------------
+//                         HIDE ITEMS
+
+const hideItem = document.getElementById('hide');
+
+hideItem.addEventListener('click', function(){
+     const label = document.querySelector('label');
+     label.textContent = "Unhide notes"
+
+      if(hideItem.checked){
+        ul.style.display ='none';
+      } else {
+        label.textContent = 'Hide notes'
+        ul.style.display = 'block';
+      }
+});
+
+//------------------------------------------------------
+//                          Search Filter
+
+const searchInput = document.querySelector('#search-note input')
+
+searchInput.addEventListener('keyup', function(e){
+
+  const searchChar = e.target.value.toUpperCase();
+  const notes = ul.getElementsByTagName('li');
+
+   Array.from(notes).forEach(function(note) {
+       const parText = note.firstElementChild.textContent;
+
+       if(parText.toUpperCase().indexOf(searchChar)!== -1){
+         note.style.display = 'block';
+       } else {
+         note.style.display = 'none';
+       }
+
+   });
+
+;});
